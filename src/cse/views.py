796af6676a0fe072ca88f91cb9f04fffd618fcc8
Shishir <sishishir2015@gmail.com>
2019-09-21 12:48:01 +0600

    Add Bootstrap And Javascript

--- src/cse/views.py
+++ src/cse/views.py
@@ -2,4 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(req):
+
+    context = {
+        ('title':'Home')
+    }
     return render (req,'cse/index.html')
